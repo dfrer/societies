fe58913998055b81c9642e54868bcc67b2c913c3
Mo <dfrer@users.noreply.github.com>
2026-04-02 14:56:32 -0700

    Add Workstation base class
    - WorkstationType enum
    - Usage tracking (IsInUse, UsingPlayerId)
    - CanUse/StartUsing/StopUsing

--- Unity/Assets/Societies/Runtime/Crafting/Workstation.cs
+++ Unity/Assets/Societies/Runtime/Crafting/Workstation.cs
@@ -1,34 +1,37 @@
 using UnityEngine;
 
-public enum WorkstationType
-{
-    None,
-    Workbench,
-    Furnace,
-    Anvil,
-    Campfire
-}
+public enum WorkstationType { None, Workbench, Furnace, Anvil, Campfire }
 
 public class Workstation : MonoBehaviour
 {
     public WorkstationType Type = WorkstationType.None;
     public string StationId { get; private set; }
+    
     public bool IsInUse { get; private set; }
-
+    public string UsingPlayerId { get; private set; }
+    
     private void Awake()
     {
         StationId = System.Guid.NewGuid().ToString();
     }
-
-    public bool CanUse(string playerId) => !IsInUse;
-
+    
+    public bool CanUse(string playerId)
+    {
+        return !IsInUse || UsingPlayerId == playerId;
+    }
+    
     public void StartUsing(string playerId)
     {
-        IsInUse = true;
+        if (CanUse(playerId))
+        {
+            IsInUse = true;
+            UsingPlayerId = playerId;
+        }
     }
-
+    
     public void StopUsing()
     {
         IsInUse = false;
+        UsingPlayerId = null;
     }
 }
